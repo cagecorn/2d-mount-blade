4fdba4c247851f2589f8ed2bbab5aef895ef58e6
cagecorn <lmcbv@naver.com>
2025-06-26 23:57:59 +0900

    refactor: add AudioManager and GameContext

---
+++ src/managers/audioManager.js
@@ -0,0 +1,36 @@
+// src/managers/audioManager.js
+
+import { context } from '../gameContext.js';
+
+export class AudioManager {
+    constructor() {
+        this.bgm = null;
+        this.isBgmPlaying = false;
+    }
+
+    init() {
+        // BGM 에셋이 로드되었다고 가정
+        this.bgm = context.assets?.bgm;
+        if (this.bgm) {
+            this.bgm.loop = true;
+            this.bgm.volume = 0.5;
+        }
+    }
+
+    startBGM() {
+        if (this.bgm && !this.isBgmPlaying) {
+            this.bgm.play().then(() => {
+                this.isBgmPlaying = true;
+                console.log("BGM started.");
+            }).catch(e => console.error("BGM play failed:", e));
+        }
+    }
+
+    stopBGM() {
+        if (this.bgm) {
+            this.bgm.pause();
+            this.bgm.currentTime = 0;
+            this.isBgmPlaying = false;
+        }
+    }
+}
