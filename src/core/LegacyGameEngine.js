8e6972dca4f4dd40348fc02438f278b7becba33a
cagecorn <lmcbv@naver.com>
2025-07-02 02:09:15 +0900

    Revert "Implement arena map support and player spawn helper"

--- src/core/LegacyGameEngine.js
+++ src/core/LegacyGameEngine.js
@@ -1,6 +1,5 @@
 import { EventBinder } from './EventBinder.js';
 import { MapManager } from '../map.js';
-import { ArenaMapManager } from '../arenaMap.js';
 import { AquariumMapManager } from '../aquariumMap.js';
 
 export class LegacyGameEngine {
@@ -122,9 +121,7 @@ export class LegacyGameEngine {
     console.log(`[Game] 맵 로딩 시작: ${mapId}`);
     eventManager?.publish('before_map_load');
 
-    this.game.mapManager = mapId === 'arena'
-      ? new ArenaMapManager()
-      : (mapId === 'aquarium' ? new AquariumMapManager() : new MapManager());
+    this.game.mapManager = mapId === 'aquarium' ? new AquariumMapManager() : new MapManager();
     if (this.game.pathfindingManager) this.game.pathfindingManager.mapManager = this.game.mapManager;
     if (this.game.motionManager) this.game.motionManager.mapManager = this.game.mapManager;
     if (this.game.movementManager) this.game.movementManager.mapManager = this.game.mapManager;
@@ -133,7 +130,7 @@ export class LegacyGameEngine {
     this.game.entityManager?.clearAll?.();
     this.game.factory?.createMapTiles?.(this.game.mapManager, this.game.entityManager);
 
-    if (mapId === 'arena') {
+    if (mapId === 'aquarium') {
       this.game.arenaEngine.start();
     } else {
       this.game.arenaEngine.stop();
